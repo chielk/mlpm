2a19b32ebe2944009b78ca630441d4cd3a1dfcf9
Chiel Kooijman <chiel999@gmail.com>
2013-10-31 15:26:38 +0100

    Code from assignment added to python file

---
+++ lab01/lab01_cremers_kooijman_nugteren.py
@@ -0,0 +1,74 @@
+# Signal generators
+def sawtooth(x, period=0.2, amp=1.0, phase=0.):
+    return (((x / period - phase - 0.5) % 1) - 0.5) * 2 * amp
+
+def sine_wave(x, period=0.2, amp=1.0, phase=0.):
+    return np.sin((x / period - phase) * 2 * np.pi) * amp
+
+def square_wave(x, period=0.2, amp=1.0, phase=0.):
+    return ((np.floor(2 * x / period - 2 * phase - 1) % 2 == 0).astype(float) - 0.5) * 2 * amp
+
+def triangle_wave(x, period=0.2, amp=1.0, phase=0.):
+    return (sawtooth(x, period, 1., phase) * square_wave(x, period, 1., phase) + 0.5) * 2 * amp
+
+def random_nonsingular_matrix(d=2):
+    """
+    Generates a random nonsingular (invertible) matrix if shape d*d
+    """
+    epsilon = 0.1
+    A = np.random.rand(d, d)
+    while abs(np.linalg.det(A)) < epsilon:
+        A = np.random.rand(d, d)
+    return A
+
+def plot_signals(X):
+    """
+    Plot the signals contained in the rows of X.
+    """
+    figure()
+    for i in range(X.shape[0]):
+        ax = plt.subplot(X.shape[0], 1, i + 1)
+        plot(X[i, :])
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+# Generate data
+num_sources = 5
+signal_length = 500
+t = linspace(0, 1, signal_length)
+S = np.c_[sawtooth(t), sine_wave(t, 0.3), square_wave(t, 0.4), triangle_wave(t, 0.25), np.random.randn(t.size)].T
+
+plot_signals(S)
+
+C = np.eye(5)  # Dummy matrix; compute covariance here
+ax = imshow(C, cmap='gray', interpolation='nearest')
+
+
+import scipy.io.wavfile
+def save_wav(data, out_file, rate):
+    scaled = np.int16(data / np.max(np.abs(data)) * 32767)
+    scipy.io.wavfile.write(out_file, rate, scaled)
+
+
+# Load audio sources
+source_files = ['beet.wav', 'beet9.wav', 'beet92.wav', 'mike.wav', 'street.wav']
+wav_data = []
+sample_rate = None
+for f in source_files:
+    sr, data = scipy.io.wavfile.read(f, mmap=False)
+    if sample_rate is None:
+        sample_rate = sr
+    else:
+        assert(sample_rate == sr)
+    wav_data.append(data[:190000])  # cut off the last part so that all signals have same length
+
+# Create source and measurement data
+S = np.c_[wav_data]
+plot_signals(S)
+
+# Requires your function make_mixtures
+#X = make_mixtures(S, make_random_nonsingular(S.shape[0]))
+#plot_signals(X)
+# Save mixtures to disk, so you can listen to them in your audio player
+#for i in range(X.shape[0]):
+#    save_wav(X[i, :], 'X' + str(i) + '.wav', sample_rate)
